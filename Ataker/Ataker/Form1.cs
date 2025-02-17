038b48e92e25001eba4decbab11dd46971e83019
xumones <toastpork13@gmail.com>
2025-02-17 18:28:30 +0700

    Level update #1 & +Class

--- Ataker/Ataker/Form1.cs
+++ Ataker/Ataker/Form1.cs
@@ -6,12 +6,12 @@ namespace Ataker
 {
     public partial class Form1 : Form
     {
-        private const int GridWidth = 10;
-        private const int GridHeight = 5;
+        private const int GridWidth = 14;
+        private const int GridHeight = 7;
         private const int Layer = 2;
-        private const int TileSize = 154;
+        private const int TileSize = 110;
 
-        private GameObject[,,] grid = new GameObject[GridWidth, GridHeight,Layer];
+        private GameObject[,,] grid = new GameObject[GridWidth, GridHeight, Layer];
         private Player player;
 
         private void Form1_Load(object sender, EventArgs e)
@@ -39,23 +39,63 @@ namespace Ataker
                         grid[x, y, l] = null;
 
             // Create player
-            player = new Player(0, 0, 1);
+            player = new Player(1, 3, 1);
             grid[player.X, player.Y, 1] = player;
 
+            //Create ProfLittle
+            grid[7, 1, 1] = new ProfLittle(7, 1, 1);
+
             // Create walls
-            grid[2, 2, 1] = new Wall(2, 2, 1);
-            grid[3, 3, 1] = new Wall(3, 3, 1);
+            for (int y = 0; y < GridHeight; y++)
+            {
+                for (int x = 0; x < GridWidth; x++)
+                {
+                    for (int l = 0; l < Layer; l++)
+                    {
+                        if (x == 0 || x == GridWidth - 1 || y == 0 || y == GridHeight - 1)
+                        {
+                            grid[x, y, l] = new Wall(x, y, l);
+                        }
+                    }
+                }
+            }
+            grid[1, 1, 1] = new Wall(1, 1, 1);
+            grid[2, 4, 1] = new Wall(2, 4, 1);
+            grid[5, 5, 1] = new Wall(5, 5, 1);
+            grid[6, 1, 1] = new Wall(6, 1, 1);
+            grid[7, 2, 1] = new Wall(7, 2, 1);
+            grid[9, 3, 1] = new Wall(9, 3, 1);
+            grid[10, 5, 1] = new Wall(10, 5, 1);
+            grid[11, 4, 1] = new Wall(11, 4, 1);
+            grid[11, 5, 1] = new Wall(11, 5, 1);
+            grid[12, 1, 1] = new Wall(12, 1, 1);
+            grid[12, 3, 1] = new Wall(12, 3, 1);
+            grid[12, 4, 1] = new Wall(12, 4, 1);
+            grid[12, 5, 1] = new Wall(12, 5, 1);
 
             // Create Obstacle
-            grid[1, 1, 1] = new DocumentPile(1, 1, 1);
+            grid[3, 2, 1] = new DocumentPile(3, 2, 1);
+            grid[3, 4, 1] = new DocumentPile(3, 4, 1);
             grid[4, 4, 1] = new DocumentPile(4, 4, 1);
+            grid[5, 2, 1] = new DocumentPile(5, 2, 1);
+            grid[6, 3, 1] = new DocumentPile(6, 3, 1);
+            grid[7, 4, 1] = new DocumentPile(7, 4, 1);
+            grid[8, 2, 1] = new DocumentPile(8, 2, 1);
+            grid[8, 5, 1] = new DocumentPile(8, 5, 1);
+            grid[10, 3, 1] = new DocumentPile(10, 3, 1);
+            grid[11, 2, 1] = new DocumentPile(11, 2, 1);
 
             // Create Trap
-            grid[7, 3, 0] = new Trap(7, 3, 0);
+            grid[3, 3, 0] = new Trap(3, 3, 0);
+            grid[3, 5, 0] = new Trap(3, 5, 0);
+            grid[4, 1, 0] = new Trap(4, 1, 0);
+            grid[7, 5, 0] = new Trap(7, 5, 0);
+            grid[9, 2, 0] = new Trap(9, 2, 0);
+            grid[9, 4, 0] = new Trap(9, 4, 0);
 
             // Create Monster
-            grid[5, 1, 1] = new Monster(5, 1, 1, 3);
-            grid[6, 3, 1] = new Monster(6, 3, 1, 3);
+            grid[2, 2, 1] = new Monster(2, 2, 1, 3);
+            grid[7, 3, 1] = new Monster(7, 3, 1, 3);
 
             Invalidate(); // Redraw the form
         }
@@ -102,6 +142,8 @@ namespace Ataker
                             g.FillRectangle(Brushes.LightBlue, tile);
                         else if (grid[x, y, l] is Trap)
                             g.FillRectangle(Brushes.LightPink, tile);
+                        else if (grid[x, y, l] is ProfLittle)
+                            g.FillRectangle(Brushes.Yellow, tile);
                         else if (l == 0) // layer 0 วาด grid
                             g.DrawRectangle(Pens.Black, tile); // Draw empty grid
                     }
---
+++ Ataker/Ataker/ProfLittle.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ataker
+{
+    public class ProfLittle : GameObject
+    {
+        public ProfLittle(int x, int y, int z) : base(x, y, z)
+        {
+
+        }
+        public override void Draw(Graphics g, int tileSize)
+        {
+            g.FillRectangle(Brushes.Yellow, X * tileSize, Y * tileSize, tileSize, tileSize); // Prof สีเหลือง
+        }
+    }
+}
